b908e82f2b92d417ab536a565b8771dc14c646d2
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-07 09:39:26 +0800

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split("language")
-print(str[0])
+str = str[26:55]
+print(str)
