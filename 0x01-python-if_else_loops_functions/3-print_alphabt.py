34b1feed2bbd6a185db9ed05e4720f330a042547
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-07 18:48:02 +0800

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    if (ch != 101 or ch != 113):
-        print("{}".format(chr(ch)), end="")
+    if (ch == 101 or ch == 113):
+        continue
+   print("{}".format(chr(ch)), end="")
