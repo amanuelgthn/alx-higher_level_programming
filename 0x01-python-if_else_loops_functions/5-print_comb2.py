6d0851a592c6f183d2e15b7e7f844cac279c7e03
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-07 19:52:03 +0800

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for decimal in range(0, 99):
-    hexnum = hex(decimal)
-    print("{} = {}".format(decimal, hexnum))
+    if decimal < 10:
+        print("{}".format(0), end="")
+    print("{}, ".format(decimal))
