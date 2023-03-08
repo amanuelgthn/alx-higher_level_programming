2e4eba03c0b3bc090a14244fc9da875d0da3aabc
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-08 11:24:01 +0800

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     i = 1
-    if b < 0:
+     if b % 2 == 0:
+        a = abs(a)
+     if b < 0:
         b = -b
         a = 1 / a
-    if b % 2 == 0:
-        a = abs(a)
     while i < b:
         a = a * a
         i = i + 1
