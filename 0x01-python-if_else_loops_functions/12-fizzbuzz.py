18d3d1d45119238fbc710d4f729e17ebb16325f5
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-08 11:44:34 +0800

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,6 +9,4 @@ def fizzbuzz():
             print("Buzz", end="")
         else:
             print("{}".format(decimal), end="")
-        if decimal != 100:
-            print(" ", end="")
-  
+        print(" ", end="")
