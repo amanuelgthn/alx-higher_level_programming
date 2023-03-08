d7ccc23ffe0754fab2b6641566b4432c64737329
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-08 09:52:45 +0800

    Create 6-print_comb3_dup.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3_dup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for decimal in range(0, 10):
+    for last_digit in range(0, 10):
+        if(last_digit > decimal):
+            print("{}".format(decimal),end="")
+            print("{}".format(last_digit),end="")
+            if( decimal == 8 and last_digit == 9):
+                continue
+            else:
+                print(", ",end="")
