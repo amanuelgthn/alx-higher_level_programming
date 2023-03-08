2afef0aed68f9f794d10a40f02a52e4198dca7f4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-08 11:41:52 +0800

    Create 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for decimal in range(1, 101):
+        if decimal % 3 == 0 and decimal % 5 == 0:
+            print("FizzBuzz", end="")
+        elif decimal % 3 == 0:
+            print("Fizz", end="")
+        elif decimal % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{}".format(decimal), end="")
+        if decimal != 100:
+            print(", ", end="")
+  
