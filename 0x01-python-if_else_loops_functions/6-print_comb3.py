b25c5fc431104ac6e4d02b62c2678e972c20f60f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-08 09:48:16 +0800

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for decimal in range(0, 10):
         if(last_digit > decimal):
             print("{}".format(decimal),end="")
             print("{}".format(last_digit),end="")
-            if( decimal == 8 and last_digit = 9):
+            if( decimal == 8 and last_digit == 9):
                 continue
             else:
                 print(", ",end="")
