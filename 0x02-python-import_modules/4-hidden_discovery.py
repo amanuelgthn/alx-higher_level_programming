72c883720af62b7b55bf06da8ae30d4f2445dccc
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-10 15:41:00 +0800

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     len_list = len(list)
     for i in range(0, len_list):
         str = list[i]
-        if (str[0] != "__"):
+        if (str[0] != "_"):
             list_data.append(list[i])
             print(list_data[j])
             j= j + 1
