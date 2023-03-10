a6e1a458ada2bb9b0dbaf7c43ad225af9cb66d53
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-10 10:34:45 +0800

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i])
