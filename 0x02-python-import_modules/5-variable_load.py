656f6b5d245c9b44cbbd5eab5bb26947d6f17649
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-10 16:04:28 +0800

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-  from variable_load_5 import a 
-  b = a
-  print(b)
+if __name__ == "__main__":\
+    from variable_load_5 import a
+    b = a
+    print(b)
