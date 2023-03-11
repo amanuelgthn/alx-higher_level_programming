aea8ab7dc6435183e56b0d6cb142da215cd80cb7
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-11 16:00:40 +0800

    Create 0-main.py

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,6 @@
+guillaume@ubuntu:~/0x03$ cat 0-main.py
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
