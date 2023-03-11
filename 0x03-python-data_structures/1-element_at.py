c92b028db1bab295d971c58abe6ca58bc73eecc1
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-11 16:18:13 +0800

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    len_list = len(my_list)
+    if idx < 0 or idx > len_list:
+        return None
+    element = my_list[idx]
+    print("Element at index {} is {}".format(idx, element))
