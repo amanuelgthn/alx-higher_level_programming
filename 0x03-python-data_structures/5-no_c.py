5a77aff9d3e48fb47c369c13115ff2c9d5b81d7b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-12 10:18:54 +0800

    Create 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     str_list = my_string.split()
     str_len = len(str_list)
     new_str = []
-    for i in str_list
+    for i in str_list:
         if i != 'c' or i != 'C':
             new_str = append.(i)
     return new_str.join()
