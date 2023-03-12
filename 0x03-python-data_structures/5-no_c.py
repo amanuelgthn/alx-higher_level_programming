9f9e28bbceee8bc5de294929ff20b3d3c98b3983
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-12 10:34:12 +0800

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     str_len = len(my_string)
     new_str = ""
     for i in my_string:
-        if i != 'C':
+        if i != 'C' and i != 'c':
             new_str = new_str + i
     return new_str
