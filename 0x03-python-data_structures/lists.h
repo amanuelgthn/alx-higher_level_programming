14a389625ef61382e586fec02f1101e957ef9fde
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-14 10:21:08 +0800

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,7 +21,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-size_t num_nodes(const listint_t *h);
+int num_nodes(const listint_t *h);
 int *array(const listint_t *h);
 int check_palindrome(int arr[], int start, int end);
 int is_palindrome(listint_t **head);
