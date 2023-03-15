973112df9d10c9bdf2df2c19e44d9de4845a48db
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-15 20:04:37 +0800

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,6 @@ def search_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             new_list.append(replace)
-        new_list.append(i)
+        else:
+            new_list.append(i)
     return new_list
