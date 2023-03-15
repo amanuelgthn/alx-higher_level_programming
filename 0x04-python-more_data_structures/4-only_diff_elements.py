ccfacb5906edd00937e60977dc012598341d47ce
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-15 20:47:29 +0800

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    list = []
+    for val in set_1:
+        for element in set_2:
+            if val == element:
+                list.append(val)
+    return list
 def only_diff_elements(set_1, set_2):
+    list = common_elements(set_1, set_2)
     x = set()
     for element in set_1:
-        x.add(element)
+        if element not in list:
+            x.add(element)
     for element in set_2:
-        x.add(element)
-    list = []
-    for element in x:
-        list.append(element)
-    return list
+        if element not in list:
+            x.add(element)
+    return x
