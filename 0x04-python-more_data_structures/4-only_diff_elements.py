18aaf6b630da438650772dc55ad60ff89a8d3d0a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-15 20:30:49 +0800

    Create 4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+def only_diff_elements(set_1, set_2):
+    x = set()
+    for element in set_1:
+        x.add(element)
+    for element in set_2:
+        x.add(element)
+    list = []
+    for element in x:
+        list.append(element)
