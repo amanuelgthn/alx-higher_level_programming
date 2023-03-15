804785a11e225962b51c0d9b5b7226b7b2103816
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-15 17:47:38 +0800

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     k =0
-    list=[[]]
+    list=[]
     for i in matrix:
         for j in i:
-            list.append(j*j)
+            list[0].append(j*j)
     return list
