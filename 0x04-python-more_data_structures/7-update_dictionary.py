25f2ca9d0b5dba10e6e78368596dc64d82d4b6f4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-16 09:26:45 +0800

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     key_values = {key:value}
-    a_dictionary[key] = value
+    a_dictionary.update(key_values)
+    return a_dictionary
