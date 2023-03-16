3363e81f0b74f35bd367dd982ce055bd61775b23
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-16 13:47:38 +0800

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     x = 0
+    dict = {}
     if len(a_dictionary) != 0:
         for keys in a_dictionary.keys():
             x = a_dictionary[keys]
-            a_dictionary[keys] = int(2 * x)
-    return a_dictionary
+            dict[keys] = int(2 * x)
+    return dict
