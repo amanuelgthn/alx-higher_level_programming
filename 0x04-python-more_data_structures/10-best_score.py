24d53dd76bd33472b57e364b28631761ec5a5949
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-19 16:29:05 +0800

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     x = 0
     y = 0
     Name = None
-    if len (a_dictionary) != 0 and a_dictionary:
+    if len (a_dictionary) != 0:
         for keys in a_dictionary.keys():
             y = a_dictionary[keys]
             if x < y:
