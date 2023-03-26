62a1c924fcd87815387ab932741fa18ac7e51dd6
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-26 17:10:00 +0800

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,10 +8,14 @@ def roman_to_int(roman_string):
             "D":500,
             "M":1000}
     sum = 0
+    length = len(roman_string)
     if(str(roman_string)):
-        for num in roman_string:
-            if num in dict:
-                sum = sum + dict[num]
+        for i in range(1,length):
+            if roman_string[i] in dict:
+                if dict[roman_string[i-1]] > dict[roman_string[i]]:
+                    sum = sum + dict[roman_string[i-1]]
+                else:
+                     sum = sum - dict[roman_string[i-1]]
             else:
                 return 0
         return sum
