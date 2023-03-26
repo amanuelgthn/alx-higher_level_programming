7601db4321a158753de8efacb67ba2315664aaca
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-26 16:16:24 +0800

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-  I	V	X	L	C	D	M
-1	5	10	50	100	500	1000
+    dict = {"I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000}
+    list = split(roamn_string)
+    return list
