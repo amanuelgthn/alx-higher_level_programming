a5e095cf81d911f65b440ded97d84c1fc017e3e6
amanuelgthn <amanuelgthn@gmail.com>
2023-03-27 03:06:44 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@ def safe_print_list(my_list=[], x=0):
     num_print = 0
     for i in my_list:
         try:
-            while num_print < x:
+            if num_print < x:
                 print(i)
                 num_print += 1
         except:
