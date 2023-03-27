cd99cc8c0751ef9f1a86c161ed7df8bf5fc8aad9
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-27 19:52:25 +0800

    Create 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/ur/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        print("Inside result: {:d}".format(result))
+    except ZeroDivisionError:
+        pass
+    finally:
+        return result
+        
+        
+        
+        
+  
