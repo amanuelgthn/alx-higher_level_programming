02d424aa3521a2d84ade11f1ea0463a3fb8671ec
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-27 19:22:16 +0800

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if num_print < x:
                 print("{:d}".format(value), end="")
                 num_print += 1
-        except (RuntimeError, TypeError, NameError, ValueError, Index Error):
+        except (RuntimeError, TypeError, NameError, ValueError, IndexError):
             break
     print()
     return num_print
