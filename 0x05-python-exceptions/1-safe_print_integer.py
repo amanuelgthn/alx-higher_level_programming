34f7912d1aa5c1223bc98fb43feef8c7adfff5c6
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-27 18:58:50 +0800

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except Error:
+    except (RuntimeError, TypeError, NameError, ValueError):
         return False
     else:
         return True
