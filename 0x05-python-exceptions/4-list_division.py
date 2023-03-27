841069f74227e631e584ea915c2ca4ccad640d7a
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-27 22:33:34 +0800

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    result = 0
+    try:
+        result = a / b
+        print("Inside result: {:d}".format(result))
+    except ZeroDivisionError:
+        pass
+    finally:
+        return 
