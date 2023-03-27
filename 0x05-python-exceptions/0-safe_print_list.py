de02ddf10b5782de4b470bb4f8d37a97204f0bd0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-27 18:18:17 +0800

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             if num_print < x:
                 print("{}".format(i), end="")
                 num_print += 1
-        except:
+        except IndexError:
             break
     print()
     return num_print
