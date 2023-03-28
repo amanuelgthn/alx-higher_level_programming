f891f1a846edad6d994d58a60935dafefa0e85c2
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-28 09:58:55 +0800

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = 0
+    list=[]
     a = 0
     b = 0
+    result = 0
     len1 = len (my_list_1)
     len2 = len (my_list_2)
     index = len1
     if index < len2:
         index = len2
-    for i in range(0, index):
-        
-    try:
-        result = a / b
-        print("Inside result: {:d}".format(result))
-    except ZeroDivisionError:
-        pass
-    finally:
-        return 
+    while i < index && j < index:
+        try:
+            a = my_list_1[i]
+            i+=1
+        except:
+            pass
+        try:
+            b = my_list_2[i]
+        except:
+            pass
+        try:
+            result = a / b
+            list.append(result)
+            list_length += 1
+        except:
+            pass
+    return list         
