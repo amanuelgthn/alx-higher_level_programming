5ca66da0a9da17c93975534735f15d9c22c9e061
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-28 13:10:59 +0800

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (RuntimeError, TypeError, NameError, ValueError):
+        print("ValueError", file=sys.stderr)
         return False
     else:
         return True
