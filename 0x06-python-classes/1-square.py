0d7540f1032277b9d93533df5b8a19cd94aa09d4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 02:25:53 +0800

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class Square with private instance size"""
+
+
+class Square:
+    """Class Square with private instance size"""
+    def __init__(self, size):
+        self.__size= size
