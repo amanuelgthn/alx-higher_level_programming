a61cab4683206698b728aab9892758dbb55d75d3
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 13:44:51 +0800

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,11 +12,26 @@ class Square:
         else:
             self.__size = size
 
-    def area(self):
-        """Public instance method to return area of square"""
-        return self.__size * self.__size
+    
     def size(self):
-        return self.__size
+         if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return self.__size
     def size(self, value):
-        self.__size = value
+         if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+         if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return self.__size * self.__size
   
