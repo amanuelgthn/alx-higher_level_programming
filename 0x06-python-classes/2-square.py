cbf4229baa5c1359bc19e2b1e65b0004814f4448
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 08:50:09 +0800

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
             if isinstance(size) == False:
                 raise TypeError("size must be an integer")
-            if size < 0:
+            elif size < 0:
                  raise ValueError("size must be >= 0")
             else:
                 self.__size = size
