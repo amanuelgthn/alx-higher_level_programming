9310b06a09c394d7bb2ceb8c9e0e5ab9e90c51d5
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 09:16:02 +0800

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Square with private instance size"""
+
+
+class Square:
+    """Class Square with private instance size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        return size * size
+    
