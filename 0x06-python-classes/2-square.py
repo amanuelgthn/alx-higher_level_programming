78d285f7e69ad16d824fb082b0ab1ba37d19d451
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 08:23:15 +0800

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,11 @@
 
 class Square:
     """Class Square with private instance size"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         try:
             num = int(size)
-            if num < 0：
-            
+            if num < 0:
+                raise Exception("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
+        self.__size = size
