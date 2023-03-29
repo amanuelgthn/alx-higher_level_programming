15e9905df86932f06528034f01accb59a7d57c7f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-29 08:04:25 +0800

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Square with private instance size"""
+
+
+class Square:
+    """Class Square with private instance size"""
+    def __init__(self, size):
+        try:
+            num = int(size)
+            if num < 0：
+            
