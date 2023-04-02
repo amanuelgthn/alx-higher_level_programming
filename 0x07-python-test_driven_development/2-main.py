87ec42c1496a7a2a3ffa2ca2e26454cee0a3a0f6
amanuelgthn <amanuelgthn@gmail.com>
2023-04-02 22:26:23 +0800

    2-matrix_divided.py

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+test1 = [[1, 2, 3, 4, 6, 7, 8],[4, 5, 6, 8, 6, 7, 6],[3, 4, 5, 9, 8, 9, 10]]
+test2 = [[1, 2, 3, 4, 6, 7, 8],
+         [4, 5, 6, 8, 6, 7, 6],
+         [3, 4, 5, "l", 8, 9, 10]]
+print(matrix_divided(test1, 9.5))
+print(matrix_divided(test1, 5))
+print(matrix_divided(test1, 3))
+print(matrix_divided(test2, 9.5))
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module containing a function that divides all elements of a matrix
+matrix must be a list of integers or floats
+div must be a number(integer or float) and can't be equalt to 0
+raise TypeError or ZeroDivisionError for the conditions respectively
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix
+    """
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) not in [int,float]:
+        raise TypeError("div must be a number")
+    if matrix:
+        if type(matrix[0]) != list:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats") 
+        len_row = len(matrix[0])
+        for row in matrix:
+            if type(row) != list:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats") 
+            elif len(row) != len_row:
+                raise TypeError("Each row of the matrix must have the same size")
+            for i in row:
+                if type(i) not in [int,float]:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    else:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    new=[]
+    for row in matrix:
+        column = []
+        for i in range(0,len_row):
+            column.append(round(row[i]/div,2))
+        new.append(column)
+    return new
+                
