a06101ff20c58b783e3834421a40cef6eaa137bc
amanuelgthn <amanuelgthn@gmail.com>
2023-04-03 20:03:07 +0800

    Create 2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Module for Class Rectangle
+class Rectangle that defines a width and height
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle that defines a width and height
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Instantiation with optional width and height of a rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Property for private instance attribute:width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Property setter for private instance attribute:width
+        """
+        if(type(value)) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Property for private instance attribute:height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Property setter for private instance attribute:height
+        """
+        if(type(value)) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return 2*(self.height + self.width)
