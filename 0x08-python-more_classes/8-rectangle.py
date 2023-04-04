94d78c12fb62314c843e0b784260e8f2dcfb7978
amanuelgthn <amanuelgthn@gmail.com>
2023-04-04 20:42:36 +0800

    Create 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,15 +88,20 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    def __ge__(self, other):
-        return self.area >= other.area
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __gt__(self, other):
+        return self.area > other.area
 
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area >= rect_2.area:
+        if rect_1.area == rect_2.area:
+            return rect_1
+        if rect_1.area > rect_2.area:
             return rect_1
         else:
             return rect_2
