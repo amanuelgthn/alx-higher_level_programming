be6bb6599e7a086ed7c93686d7bbb40ed0e8094e
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-06 10:36:07 +0800

    Create 101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This Module solves the N queens problem
+for N greater than or equal to 4
+"""
+
+
+import sys
+
+if __name__ === "__main__":
+    if len(sys.argv) != 2:
+        print("101-nqueens.py")
+        sys.exit(1)
+    print(type(sys.argv))
+      
