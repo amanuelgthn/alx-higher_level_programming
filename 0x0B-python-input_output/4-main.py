4c3006f562e009703cc836e86a9c8d88a6ecb40e
amanuelgthn <amanuelgthn@gmail.com>
2023-04-12 23:30:01 +0800

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/4-main.py
+++ 0x0B-python-input_output/4-main.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-from_json_string = __import__('4-from_json_string').from_json_string
-
-s_my_list = "[1, 2, 3]"
-my_list = from_json_string(s_my_list)
-print(my_list)
-print(type(my_list))
-
-s_my_dict = """
-{"is_active": true, "info": {"age": 36, "average": 3.14}, 
-"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
 """
-my_dict = from_json_string(s_my_dict)
-print(my_dict)
-print(type(my_dict))
+Import module JSON
+"""
 
-try:
-    s_my_dict = """
-    {"is_active": true, 12 }
+
+import json
+"""
+Module containing the function save_to_json_file
+"""
+
+
+def save_to_json_file(my_obj, filename):
     """
-    my_dict = from_json_string(s_my_dict)
-    print(my_dict)
-    print(type(my_dict))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    function that writes an object to a text file,using a JSON representation
+    """
+    with open(filename,"w") as file:
+        json.dump(my_obj, file)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,9 @@ Module containing the function save_to_json_file
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename,'w', encoding="utf-8") as file:
+    """
+    function that writes an object to a text file,using a JSON representation
+    """
+    
+    with open(filename,"w", encoding="utf-8") as file:
         json.dump(my_obj, file)
