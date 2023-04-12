f0e558c239a5b579a72ef4ed5f7a280982d56444
amanuelgthn <amanuelgthn@gmail.com>
2023-04-13 02:40:02 +0800

    Create 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,14 +12,15 @@ save them to a file
 """
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    
-    filename = "add_item.json"
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
     filename = load_from_json_file(filename)
-    n = len(sys.argv)
-    list = []
-    list.extend(sys.argv[1:])
-    save_to_json_file(list, filename)
-    
+except (ValueError, FileNotFoundError):
+    list_item = []
+n = len(sys.argv)
+for i in sys.argv[1:]:
+    list_item.append(i)
+save_to_json_file(list_item, filename)
