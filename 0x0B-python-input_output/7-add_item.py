0a09db7c967c8800c30044a8bd53e24d1e86db0d
amanuelgthn <amanuelgthn@gmail.com>
2023-04-13 00:18:27 +0800

    Update

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+import json module
+"""
+
+
+import json
+import sys
+"""
+module containing
+"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_js
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
