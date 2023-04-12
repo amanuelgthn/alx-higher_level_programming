0a1e583843410699959e789648afdd9713cd34e6
amanuelgthn <amanuelgthn@gmail.com>
2023-04-12 19:44:10 +0800

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module containing append_write function
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text
+    and returns the number of characters added
+    """
+    with open(filename, 'a',encoding="utf-8") as file:
+        return file.write(text)
