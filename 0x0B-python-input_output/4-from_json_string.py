84514af73cca8493485ed904e65a509dcfc27129
amanuelgthn <amanuelgthn@gmail.com>
2023-04-12 20:48:57 +0800

    Create 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -14,4 +14,4 @@ def from_json_string(my_str):
     function that returns an object(python data structure)
     represented by a JSON string
     """
-    return json.dumps(my_str)
+    return json.loads(my_str)
