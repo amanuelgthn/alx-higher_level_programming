f27113f018a2a31d0e97ecc9300ee77206910de2
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-12 09:28:36 +0800

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,5 +9,5 @@ def inherits_from(obj, a_class):
     function that returns true if an object is
     directly or indirectly inherited from a-class
     """
-    
-    return (issubclass(obj, a_class))
+
+    return issubclass(obj, a_class)
