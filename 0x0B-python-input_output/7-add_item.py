ee37d46d7c66dbdcb0664753f5961ea66f7cf441
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-13 08:18:11 +0800

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-import sys Module
+import json module
+import sys
 """
 
 
+import json
 import sys
 """
 module containing a script that adds
@@ -15,12 +17,10 @@ save them to a file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-try:
-    filename = load_from_json_file(filename)
-except (ValueError, FileNotFoundError):
-    list_item = []
 n = len(sys.argv)
-for i in sys.argv[1:]:
-    list_item.append(i)
-save_to_json_file(list_item, filename)
+list = []
+for i in range(1, n):
+    list.append(sys.argv[i])
+filename = "add_item.json"
+save_to_json_file(list, filename)
+filename = load_from_json_file(filename)
