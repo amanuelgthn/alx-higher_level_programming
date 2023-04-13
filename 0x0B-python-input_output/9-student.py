6e0682d9a92d52d1e1b9fbe379a51f70f960d665
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-13 09:52:53 +0800

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,24 +4,23 @@ Module class Student
 """
 
 
- class Student:
+class Student:
+    """
+    student class
+    """
+    
+    
+    def __init__(self, first_name, last_name, age):
         """
-        student class
+        Initialiazation
         """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-
-        def __init__(self, first_name, last_name, age):
-            """
-            Initialiazation
-            """
-            self.first_name = first_name
-            self.last_name = last_name
-            self.age = age
-
-        def to_json(self):
-            """
-            function that returns the dictionary description
-            with simple data structure
-            """
-            return vars(self)
-      
+    def to_json(self):
+        """
+        function that returns the dictionary description
+        with simple data structure
+        """
+        return vars(self)
