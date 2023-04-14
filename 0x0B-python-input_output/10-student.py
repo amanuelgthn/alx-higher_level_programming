edabbf609e3ee9a0564e46a66a078e8a75e62fae
amanuelgthn <amanuelgthn@gmail.com>
2023-04-14 09:13:35 +0800

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module class Student
+"""
+
+
+class Student:
+    """
+    student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialiazation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None): 
+        """Public method that returns the dictionary description
+        with simple data structure
+        """
+        dict_attr = self.__dict__.copy()
+        if (type(attrs) == list and all(type(item) == str for item in attrs)):
+            att_dict = {}
+            for i in attrs:
+                if i in dict_attr:
+                    att_dict[i] = dict_attr[i]
+            return att_dict
+        return dict_attr
