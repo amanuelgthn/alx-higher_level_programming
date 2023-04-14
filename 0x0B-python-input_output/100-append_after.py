dea3d9e85364eb76733f0286efbb53c6b9aa4424
amanuelgthn <amanuelgthn@gmail.com>
2023-04-14 14:51:16 +0800

    update 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module containing append_after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    function that inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module containing Pascal's Triangle
 """
 
+
 def factorial_num(n):
     """
     function that returns the factorial of a number
@@ -13,11 +14,14 @@ def factorial_num(n):
         return 1
     return n * factorial_num(n-1)
 
-def combination(col,row):
+
+def combination(col, row):
     """
     function that returns the combination of two numbers
     """
-    return int(factorial_num(col) / factorial_num(row) / factorial_num(col - row))
+    return int(factorial_num(col) /
+               factorial_num(row) / factorial_num(col - row))
+
 
 def pascal_triangle(n):
     """
@@ -26,11 +30,10 @@ def pascal_triangle(n):
     column = []
     if n <= 0:
         return column
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         row = []
-        for j in range(0,i):
-            value = combination(i-1,j)
+        for j in range(0, i):
+            value = combination(i-1, j)
             row.append(value)
         column.append(row)
     return column
-
