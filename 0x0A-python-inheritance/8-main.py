b126a2bd65eb57a32f31bc459974c142ec372265
amanuelgthn <amanuelgthn@gmail.com>
2023-04-15 10:51:25 +0800

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('8-rectangle').Rectangle
 
 r = Rectangle(3, 5)
@@ -15,3 +16,4 @@ try:
     r2 = Rectangle(4, True)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+print(issubclass(Rectangle, BaseGeometry))
