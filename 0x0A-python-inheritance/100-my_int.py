2ef1b330ab7910d0850ef45b2049dcbe843ce35d
amanuelgthn <amanuelgthn@gmail.com>
2023-04-15 12:49:15 +0800

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,8 +9,29 @@ class MyInt(int):
     MyInt class
     """
 
-    def _eq__(self, other):
-        return self != other
+    def __init__(self, int_num):
+        """
+        Inittialization
+        """
+        self.int_num = int_num
+
+    @property
+    def int_num(self):
+        return self.__int_num
+
+    @int_num.setter
+    def int_num(self, int_num):
+        if type(int_num) is int:
+            self.__int_num = int_num
+
+    def __eq__(self, other):
+        if self.int_num == other:
+            return False
+        else:
+            return True
 
     def __ne__(self, other):
-        return self == other
+        if self.int_num != other:
+            return False
+        else:
+            return True
