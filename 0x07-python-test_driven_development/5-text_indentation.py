f4913a57173a6865d94ef30c530c2e611daaae6e
amanuelgthn <amanuelgthn@gmail.com>
2023-05-06 15:29:24 +0800

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,12 @@
 This is a module containing text_indentaion function
 """
 
+
 def split_text(text, symbol):
     """
     function that returns a list of split text with the given symbols
     """
-    result =[]
+    result = []
     curr_char = ""
     for char in text:
         if char in symbol:
@@ -19,19 +20,20 @@ def split_text(text, symbol):
                 continue
             else:
                 curr_char += char
-    if curr_char:
-        result.append(curr_char)
+    #if curr_char:
+        #result.append(curr_char)
     return result
+
 
 def text_indentation(text):
     """
-    Function that prints a text with 2 new lines after each 
+    Function that prints a text with 2 new lines after each
     these characters: ., ? and :
     """
     if type(text) != str:
         raise TypeError("text must be a string")
     else:
         list_split = []
-        list_split = split_text(text, ["." , ":" , "?"])
+        list_split = split_text(text, [".", ":", "?"])
         for item in list_split:
             print(item)
