8e524c27385832c4c07e7593e110cfa5eeaeae6f
amanuelgthn <amanuelgthn@gmail.com>
2023-05-06 14:51:56 +0800

    Create 5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This is a module containing text_indentaion function
+"""
+
+def split_text(text, symbol):
+    """
+    function that returns a list of split text with the given symbols
+    """
+    result =[]
+    curr_char = ""
+    for char in text:
+        if char in symbol:
+            if curr_char:
+                result.append(curr_char+char)
+                curr_char = ""
+        else:
+            #if char != " ":
+            curr_char += char
+    if curr_char:
+        result.append(curr_char)
+    return result
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after each 
+    these characters: ., ? and :
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        list_split = []
+        list_split = split_text(text, ["." , ":" , "?"])
+        for item in list_split:
+            print(item)
