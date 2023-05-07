04d8060f4a101f91ab3efabdeb43da42494ef3dd
amanuelgthn <amanuelgthn@gmail.com>
2023-05-07 15:24:02 +0800

    Update 100-matrix_mul.py, tests/100-matrix_mul.txt

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" Doc """
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+m_a = [[1, 2], [3, 4], [3, 4]]
+m_b = [[5, 6, 1], [7, 8, 2]]
+print(matrix_mul(m_a, m_b))
--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -20,12 +20,14 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise TypeError("m_b can't be empty")
-    if any(not isinstance(row[i], (float, int))
-           for row in m_a for i in range(0, len(m_a)-1)):
-        raise TypeError("m_b should contain only integers or floats")
-    if any(not isinstance(row[i], (float, int))
-           for row in m_b for i in range(0, len(m_b)-1)):
-        raise TypeError("m_b should contain only integers or floats")
+    for row in m_a:
+        for element in row:
+            if not isinstance(element, (float, int)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for element in row:
+            if not isinstance(element, (float, int)):
+                raise TypeError("m_b should contain only integers or floats")
     if not all(len(m_a[i-1]) == len(m_a[i]) for i in range(1, len(m_a))):
         raise TypeError("each row of m_a must be of the same size")
     if not all(len(m_b[i-1]) == len(m_b[i]) for i in range(1, len(m_b))):
