e2dfea9fd5e89e17b5e25309ec401b693854d70d
amanuelgthn <amanuelgthn@gmail.com>
2023-05-17 19:48:43 +0800

    UPDATE 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- a script that coverts htbtn_0c_0 database to UTF8 (utf8mb4,collate utf8mb4_univode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
