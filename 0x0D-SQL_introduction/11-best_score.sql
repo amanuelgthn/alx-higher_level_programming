a502fe622bf8ff47c3e733c5ca7dbd9964380bad
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-17 12:13:21 +0800

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the table second_table
-SELECT score, name FROM second_table WHERE Score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
