f1d7aa681606092da8f6b9e0681f6d21deee6441
amanuelgthn <amanuelgthn@gmail.com>
2023-05-17 23:48:00 +0800

    create 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- a script that creates the DATABASE hbtn_0d_2 and the user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'LOCALHOST' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON  'hbtn_0d_2' TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES; 
