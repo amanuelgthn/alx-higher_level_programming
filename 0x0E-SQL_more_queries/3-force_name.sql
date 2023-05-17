2191f9c30184b04b7e85d7c664e8d9c742dbb413
amanuelgthn <amanuelgthn@gmail.com>
2023-05-18 00:00:17 +0800

    Create 3-force_name.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- a script that creates the table force_name on MySQL server
+force_name description:
+id INT
+name VARCHAR(256) can’t be null
+CREATE TABLE force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
