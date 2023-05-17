3ebda0a174ff30a13fc36185def2667cfb446750
amanuelgthn <amanuelgthn@gmail.com>
2023-05-17 18:19:01 +0800

    Create 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,5 @@
 -- a script that lists the number of records with the same score in second_table
-SELECT
-    score,
-    COUNT(*) AS number
+SELECT score COUNT(*) AS number
 FROM
     second_table
 GROUP BY
