51adef600d72530991eb2fab43bd1b615d717958
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 10:35:10 +0800

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,6 +4,6 @@
 -- If a show doesn’t have a genre, display NULL
 
 SELECT tv_shows.title, tv_show_genres.genre_id COALESCE(tv_shows.id, 'Null') FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
