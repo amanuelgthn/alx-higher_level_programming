ccd89239d8b3a5524f7184470208da113f177f7d
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 16:34:16 +0800

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,8 +5,8 @@
 
 SELECT tv_genres.name AS name FROM tv_genres
 INNER JOIN tv_show_geners
-ON tv_genres.id =tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
-ON tv_shows.id = tv_genres.id
+ON tv_shows.id = tv_show_genres.genre_id
 WHERE tv_shows.tile = 'Dexter'
 ORDER BY name;
