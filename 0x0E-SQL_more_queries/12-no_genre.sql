cb75965ef8297a35e9c273025855c80adba86ee4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 10:52:29 +0800

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,7 @@
 --Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
+USE hbtn_0d_tvshows;
 SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
 ON id=tv_show_genres.show_id
