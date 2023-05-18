11b69392f48c5aab519fb1eef47bf8edd6ef4c9c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 10:54:55 +0800

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,9 @@
 -- script tht lists all shows contained in hbtn_0d_tvshows without a genre linked
---Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-USE hbtn_0d_tvshows;
-SELECT title, tv_show_genres.genre_id FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
-ON id=tv_show_genres.show_id
+ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
