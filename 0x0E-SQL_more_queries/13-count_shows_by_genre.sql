603add51ff273663be6dd725867db765f917bc48
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 13:38:43 +0800

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,8 +5,8 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 
-USE hbtn_0d_tvshows;
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id =tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC
