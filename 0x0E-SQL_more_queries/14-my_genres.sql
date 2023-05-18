e5db5b1c9e7e4b8383f4c0024479c3cbaf8363bb
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 16:33:11 +0800

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,5 +6,7 @@
 SELECT tv_genres.name AS name FROM tv_genres
 INNER JOIN tv_show_geners
 ON tv_genres.id =tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter');
+INNER JOIN tv_shows
+ON tv_shows.id = tv_genres.id
+WHERE tv_shows.tile = 'Dexter'
 ORDER BY name;
