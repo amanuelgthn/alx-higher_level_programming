808e9cee7797bb0e6e3115184c7fa8228c26e774
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-05-18 13:29:48 +0800

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,6 +7,6 @@
 
 USE hbtn_0d_tvshows;
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
-INNER JOIN TV_SHOW_
-ON tv_genres.ID =tv_genres
-ORDER BY number_of_shows
+INNER JOIN tv_show_genres
+ON tv_genres.ID =tv_show_genres.genre_id
+ORDER BY number_of_shows DESC
