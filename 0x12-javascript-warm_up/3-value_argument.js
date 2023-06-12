5992a51019c42f257bf0e5c0070ee7effa8185a5
amanuelgthn <amanuelgthn@gmail.com>
2023-06-12 20:12:54 +0800

    Create 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // script that prints the first argument passed to it
 const args = process.argv;
-if (args.length < 3) {
+if (args[2] == null) {
   console.log('No argument');
-} else if (args.length === 3) {
+} else {
   console.log(args[2]);
 }
