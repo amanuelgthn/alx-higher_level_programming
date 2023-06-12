25b973f2e3711823be637484e628250a723b8218
amanuelgthn <amanuelgthn@gmail.com>
2023-06-12 21:41:41 +0800

    create ./5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+// If the argument can’t be converted to an integer, print “Not a number”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use try/catch
+
+const args = process.argv;
+if (args.length < 3 || typeof args[2] !== 'number') {
+  console.log('Not a number');
+} else if (typeof Number(args[2]) === 'number') {
+  console.log('%s: %d', 'My Number', Number(args[2]));
+}
