189f67c3c169cd6dadef5fae013d116085c9d09a
amanuelgthn <amanuelgthn@gmail.com>
2023-06-12 19:31:13 -0700

    FIx documentation 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that prints x times "C is fun"
+// script that prints x times "C is fun"
 // Where x is the first argument of the script
 // If the first argument can’t be converted to an integer, print “Missing number of occurrences”
 // You must use console.log(...) to print all output
@@ -10,11 +10,10 @@ const args = process.argv;
 const argNum = parseInt(args[2]);
 let i = 0;
 if (isNaN(argNum)) {
-    console.log('Missing number of occurrences');
-}
-else {
-    while (i < argNum) {
-        console.log("C is fun");
-        i++;
-    }
+  console.log('Missing number of occurrences');
+} else {
+  while (i < argNum) {
+    console.log('C is fun');
+    i++;
+  }
 }
