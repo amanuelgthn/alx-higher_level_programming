42d92c3c18c962583938981835832afab739e3a0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-13 15:55:01 +0800

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that computes and prints a factorial
+// the first argument is integer(argument can be cast as integer) used for computing the factorial
+// Factorial of NaN is 1
+// Using Recursion function
+// console.log() to print all output
+function 
+numArgs = process.argv;
+num = Number(numArgs[2];
