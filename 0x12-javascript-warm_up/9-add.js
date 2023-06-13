689ec28c31817f14d76aa5f9b43b288b1b9c7de0
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-13 14:29:54 +0800

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 let argNum1 = parseInt(args[2]);
 let argNum2 = parseInt(args[2]);
-console.log(add.call(this,argNum1, argNum2)));
+console.log(add.call(this,argNum1, argNum2));
 function add(a, b) {
   return a + b;
 }
