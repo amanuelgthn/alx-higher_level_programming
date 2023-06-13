d7c199c9124dcacaedff2ae3b527b494d8355a6a
amanuelgthn <amanuelgthn@gmail.com>
2023-06-13 18:32:35 +0800

    Create 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,22 @@
 // Factorial of NaN is 1
 // Using Recursion function
 // console.log() to print all output
-function printFactorial(num) {
-  if (num === NaN)
+function printFactorial (num) {
+  if (isNaN(num) || num < 0) {
     return 1;
-  
+  }
+  if (num === 0) {
+    return 1;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  if (num === 2) {
+    return 2;
+  }
+  return num * printFactorial(num - 1);
 }
-numArgs = process.argv;
-num = Number(numArgs[2];
+const numArgs = process.argv;
+const num = Number(numArgs[2]);
+const ret = printFactorial(num);
+console.log(ret);
