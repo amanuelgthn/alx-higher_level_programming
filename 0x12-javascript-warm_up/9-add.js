19d1e6c1137658852c21dac52bba82f8f9093385
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-13 11:08:14 +0800

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 //a script that prints the addition of 2 integers
-function add(a, b) {
-  return a + b;
-}
 const args = process.argv;
 const argNum1 = parseInt(args[2]);
 const argNum2 = parseInt(args[2]);
 console.log(add(argNum1,argNum2));
+function add(a, b) {
+  return a + b;
+}
