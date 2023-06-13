4629c0e4874a641a40e23513941c4fe665f625b6
amanuelgthn <amanuelgthn@gmail.com>
2023-06-13 19:36:29 +0800

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// an empty class Rectangle that defines a rectangle
+// must use the class notation for defining your class
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
