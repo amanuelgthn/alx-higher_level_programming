7701a065a97265afe86bef12aa54a75864d21d28
amanuelgthn <amanuelgthn@gmail.com>
2023-06-13 18:48:45 -0700

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // write a function that returns the number of occurrences in a list
 exports.esrever = function (list) {
-    let arr = []
-    const lenList = list.length;
-    let i = 0;
-    while (i < lenList) {
-        arr[i] = list[lenList - i - 1]
-      i++;
-    }
-    return arr;
-  };
+  const arr = [];
+  const lenList = list.length;
+  let i = 0;
+  while (i < lenList) {
+    arr[i] = list[lenList - i - 1];
+    i++;
+  }
+  return arr;
+};
