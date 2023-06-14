24a96457e7a686c5ee713cd3ac807435d8ff3e1b
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-14 10:08:50 +0800

    Create 9-logme.js

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// function that prints the number of already printed and the new argument value
+// Output format: <number of arguments already printed>: <current argument value>
+let count = 0;
+exports.logme = function (item) {
+  count += 1;
+  console.log('%d: %s", count, item);
+              }
