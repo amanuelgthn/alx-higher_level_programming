c761a128353aba3d5fe543ced6b775cc389eba5d
amanuelgthn <amanuelgthn@gmail.com>
2023-06-13 19:34:29 -0700

    Documentation fix 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write a function that converts a number from base 10 to another base passed as argument
 // Declaring any variable (var, let) not allowed
-// importing any file not allowed 
+// importing any file not allowed
 exports.converter = function (base) {
   return num => num.toString(base);
-  };
+};
