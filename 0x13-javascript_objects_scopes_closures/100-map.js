6473b48a4d7e4ce694c0ffca502f9e34beb94fa3
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-14 10:49:29 +0800

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,7 @@
 // Must use a Map
 // A new list must be created with each value equal to the value of the initial list, multipled by the index in the list
 // Print both the initial list and the new list
-const data = require('./100-data').data;
-const newData = data.map(x => x * x);
+const data = require('./100-data.js').data;
+const newData = data.map((x, index) => x * index);
+console.log(data);
 console.log(newData);
-
