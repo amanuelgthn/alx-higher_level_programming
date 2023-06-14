639428e114b58e3ca9bb2cca30e9a7380448201b
amanuelgthn <amanuelgthn@gmail.com>
2023-06-14 20:55:40 +0800

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,5 +3,13 @@
 // import dict from the file 101-data.js
 // print the new dictionary at the end
 const dict = require('./101-data.js').dict;
-const key = Object.keys(dictionary);
-const value = Object.values(dictionary);
+const key = Object.keys(dict);
+const value = Object.values(dict);
+const newDict = {};
+for (const item of value) {
+  if (!newDict[item]) {
+    newDict[item] = [];
+  }
+  newDict[item].push(key[item]);
+}
+console.log(newDict);
