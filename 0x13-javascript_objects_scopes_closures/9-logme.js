9bd2a73d68bc5b933566132e3a0d47e462ad2b77
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-14 10:19:56 +0800

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,7 +2,7 @@
 // function that prints the number of already printed and the new argument value
 // Output format: <number of arguments already printed>: <current argument value>
 let count = 0;
-exports.logme = function (item) {
+exports.logMe = function (item) {
   count += 1;
   console.log('%d: %s', count, item);
 };
