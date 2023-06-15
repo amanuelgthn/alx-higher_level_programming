898aaecea713def8bb8bc9c232f28dcfbf2b5356
amanuelgthn <amanuelgthn@gmail.com>
2023-06-15 10:45:22 +0800

    Create 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// A script that searches the second biggest integer in the list of arguments
+// if no arguments passed print 0
+// if the number of arguments is print 0
+const args = process.argv;
+let i = 2;
+const lenArgs = args.length;
+const argList = [];
+if (lenArgs < 4) {
+  console.log(0);
+} else {
+  for (; i < lenArgs; i++) {
+    argList.push(args[i]);
+  }
+  argList.sort();
+  const lenList = argList.length;
+  console.log(argList[lenList - 2]);
+}
