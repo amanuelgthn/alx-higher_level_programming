226deda0d8bf60097933553bd1fa3cf079364847
amanuelgthn <amanuelgthn@gmail.com>
2023-06-16 15:36:39 +0800

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,15 +2,17 @@
 """
 Script that lists all states with a name starting with N from hbtn_0e_0_usa
 """
+
+
+import sys
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost', user='root', passwd='root',
-                    db='hbtn_0e_0_usa', port=3306)
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-states = cur.fetchall()
-for item in states:
-    if (item[1].startswith('N')):
-        print(item)
-db.commit()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    states = cur.fetchall()
+    for item in states:
+        if (item[1].startswith('N')):
+            print(item)
