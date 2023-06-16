61343f52862d3960a1f7071e521bb45ce2cccecc
amanuelgthn <amanuelgthn@gmail.com>
2023-06-16 16:37:05 +0800

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N from hbtn_0e_0_usa
+
 """
 
 
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    format_query = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
+    cur.execute(format_query)
     states = cur.fetchall()
     for item in states:
-        if (item[1] == (sys.argv[4])):
-            print(item)
+        print(item)
