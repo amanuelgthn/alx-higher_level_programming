f1746a5661be2314235e97d90b7c6f7cdedfce99
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-06-16 11:52:54 +0800

    Update 0-select_states.py
    Fix over-indented line for visual indent

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ The script takes 3 arguments: mysql username, mysql password & database name
 import MySQLdb
 
 db = MySQLdb.connect(host='localhost', user='aman', passwd='AAaa222!!',
-                    db='hbtn_0e_0_usa', port=3306)
+                     db='hbtn_0e_0_usa', port=3306)
 cur = db.cursor()
 cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
 states = cur.fetchall()
