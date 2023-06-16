e89de32173a1e0ec40750fdad0ff873f9476b32d
amanuelgthn <amanuelgthn@gmail.com>
2023-06-16 15:43:07 +0800

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N from hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    states = cur.fetchall()
+    for item in states:
+        if (item[1] == (sys.argv[4])):
+            print(item)
