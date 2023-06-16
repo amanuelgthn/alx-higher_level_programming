3ca8fdc6ce4bf47e6972ee1108ee623c0d09201a
amanuelgthn <amanuelgthn@gmail.com>
2023-06-16 11:58:52 +0800

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/0-create_table.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This script creates a table called `song` in the database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost', user='aman', passwd='AAaa222!!',db='hbtn_0e_0_usa')
+cur = db.cursor()
+
+# Create the database if it doesn't already exist
+cur.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa")
+
+# Drop the `song` table if it already exists
+cur.execute("DROP TABLE IF EXISTS song")
+
+# Create the `song` table
+cur.execute("CREATE TABLE song ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
+
+# Get a list of songs
+songs = ('Purple Haze', 'All Along the Watch Tower', 'Foxy Lady')
+
+# Insert each song into the `song` table
+for song in songs:
+    cur.execute("INSERT INTO song (title) VALUES(%s)", (song,))
+    print("Auto Increment ID: %s" % cur.lastrowid)
+
+# Commit the changes to the database
+db.commit()
+
+# Close the database connection
+db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N from hbtn_0e_0_usa
+"""
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost', user='aman', passwd='AAaa222!!',
+                    db='hbtn_0e_0_usa', port=3306)
+cur = db.cursor()
+cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+states = cur.fetchall()
+for item in states:
+    if (item[1].startswith('N')):
+        print(item)
+db.commit()
+db.close()
