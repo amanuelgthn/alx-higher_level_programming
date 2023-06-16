7561c6105d33b05b6311491fad58e4256a3bad4e
amanuelgthn <amanuelgthn@gmail.com>
2023-06-16 15:27:06 +0800

    Update 0-select_sates.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """
 This script lists all states from the the database `hbtn_0e_0_usa`.
 The script takes 3 arguments: mysql username, mysql password & database name
@@ -10,8 +11,9 @@ The script takes 3 arguments: mysql username, mysql password & database name
 import sys
 import MySQLdb
 
-if __name__ ==  "__main__":
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
     states = cur.fetchall()
