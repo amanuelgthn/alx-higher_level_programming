39b87f66b3c1446aa192350efffe34cb0b23593b
amanuelgthn <amanuelgthn@gmail.com>
2023-06-17 10:07:35 +0800

    Update ORDER BY cities.id 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     query = """SELECT cities.name FROM cities\
         JOIN states\
             ON cities.state_id = states.id\
-            WHERE states.name = %s"""
+            WHERE states.name = %s\
+            ORDER BY cities.id ASC"""
     cur.execute(query, (name,))
     states = cur.fetchall()
     i = 0
