46c1367116097132206f9650a8f8201c8bfc8fab
amanuelgthn <amanuelgthn@gmail.com>
2023-06-19 00:43:47 +0800

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,8 +9,7 @@ the letter a from the database hbtn_0e_6_usa
 
 
 import sys
-import MySQLdb
-from model_state import Base, State
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
@@ -20,6 +19,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = scoped_session(sessionmaker(bind=engine))
+    Base.metadata.create_all(engine)
     states = session.query(State)
     for state in states:
         if "a" in state.name:
