6980f51dd7088cbd1b016a72632b4fdb3ab640bc
amanuelgthn <amanuelgthn@gmail.com>
2023-06-18 11:02:06 +0800

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,4 +22,7 @@ if __name__ == '__main__':
         sys.argv[1], sys.argv[2], sys.argv[3]))
     session = scoped_session(sessionmaker(bind=engine))
     states = session.query(State).where(State.id == 1).first()
+    if states is None:
+        print()
+        return
     print('{}: {}'.format(states.id, states.name))
