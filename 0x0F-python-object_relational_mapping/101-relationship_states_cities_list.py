22c718bc610b0e39c60e8bf693ae95d6b77d0b36
amanuelgthn <amanuelgthn@gmail.com>
2023-06-18 23:59:23 +0800

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     for state in states:
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
+            print('    {}: {}'.format(city.id, city.name))
