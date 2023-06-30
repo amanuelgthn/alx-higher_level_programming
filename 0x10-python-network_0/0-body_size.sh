3050031351c76204133808966bb19277e9a63921
amanuelgthn <amanuelgthn@gmail.com>
2023-06-30 00:51:05 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Bash script that takes a URL,sends a request to that URL and displays the size of the body of the response
-curl -s "$1" | grep -i 'content-length'
+curl -s "$1" | grep -i "content-length"
