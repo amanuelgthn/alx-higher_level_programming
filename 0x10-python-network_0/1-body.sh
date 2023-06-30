51c93a8f009c76b5956565c0f88c310f1c60306e
amanuelgthn <amanuelgthn@gmail.com>
2023-06-30 21:48:45 +0800

    create 1-body.sh'

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL and displays the response, Displays only body of a 200 status code response
-curl -sXG "$1" -L
+curl -sL "$1"
