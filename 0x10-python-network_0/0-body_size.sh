66bb2d278bc7ba7aa8dbd79325c10b8ae5e176f7
amanuelgthn <amanuelgthn@gmail.com>
2023-06-30 16:17:09 +0800

    UPdate 0-body_size.sh use cut command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes a URL,sends a request to that URL and displays the size of the body of the response
-curl -sI "$1" | grep ^Content-Length:\d+$ | grep \d+$
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
