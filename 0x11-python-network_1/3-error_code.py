9cb91f95530e181fbfdc3ad36d05b2502a710745
amanuelgthn <amanuelgthn@gmail.com>
2023-07-01 15:36:20 +0800

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+-- displays the body of the response decoded in utf-8.
+-manage urllib.error.HTTPError exceptions
+-- print: Error code: followed by the HTTP status code
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from urllib.error import HTTPError
+    from urllib.request import Request, urlopen
+
+    url = argv[1]
+    req = Request(url)
+    try:
+        with urlopen(req) as response:
+            print(response.read())
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
