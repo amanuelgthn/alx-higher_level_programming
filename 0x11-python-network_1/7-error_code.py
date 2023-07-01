2d184c54b9cb3b43d8738db14841119e1ae4c101
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 07:17:10 +0800

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+displays the body of the response"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print('Erro code: {}'.format(response.status_code))
+    else:
+        print(response.text)
