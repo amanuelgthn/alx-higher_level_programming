325ed0480d60f00c5d1435e6e0d586531ce8f540
amanuelgthn <amanuelgthn@gmail.com>
2023-07-01 10:57:36 +0800

    create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+-Python script tha fetches https://alx-intranet.hbtn.io/status
+-using urllib
+-body of the response must be displayed (tabulation before -)
+"""
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+
+    url = "https://alx-intranet.hbtn.io/status"
+    request = Request(url)
+    with urlopen(request) as response:
+        page = response.read()
+        print("Bpdy response:")
+        print('\t- type: {}'.format(type(page)))
+        print('\t- content: {}'.format(page))
+        print('\t- utf8 content: {}'.format(page.decode('utf8')))
