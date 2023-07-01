54b6312f10b45d1bbc6a74087dde304f35c026c5
amanuelgthn <amanuelgthn@gmail.com>
2023-07-01 09:59:41 +0800

    Create 4-header.sh

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Bash script that takes in a URL as an argument, sends GET requests to the URL and displays the body of the response, Header variable X-School_User-Id must be sent with the value 98
+curl -sX -H "X-School_User-Id: 98" "$1"
