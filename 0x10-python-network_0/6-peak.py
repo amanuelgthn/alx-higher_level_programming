a151b17c0b972933aff9e611abd81a5dfa3dd1da
amanuelgthn <amanuelgthn@gmail.com>
2023-07-01 10:31:02 +0800

    Create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds a peak in a list of integers
+    """
+    for i in range(len(list_of_integers)):
+        if i > 0 and i < len(list_of_integers) - 1:
+            if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i+1]:
+                return list_of_integers[i]
