c74969c0880b7e5345331c0b9e5aabf1fe5458b4
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 07:18:25 +0800

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     url = argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print('Erro code: {}'.format(response.status_code))
+        print('Error code: {}'.format(response.status_code))
     else:
         print(response.text)
