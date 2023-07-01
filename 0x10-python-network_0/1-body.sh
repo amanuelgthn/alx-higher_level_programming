223ce8b3744b2bb22cd7c659095a1e10195e4154
amanuelgthn <amanuelgthn@gmail.com>
2023-07-01 09:08:59 +0800

    Update conditional statement

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL and displays the response, Displays only body of a 200 status code response
-if [[ $(curl -sLXW "%{http_code}" "$1") == 200 ]]; then curl -sL "$1"; fi
+if [[ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == 200 ]]; then curl -sL "$1"; fi
