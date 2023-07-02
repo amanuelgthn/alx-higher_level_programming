3ae66dd86279308d0d40f43407c7bacc932df97c
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 16:23:20 +0800

    Create 1-2-catch_me.sh

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body response
+curl -s 0.0.0.0:5000/catch_me
