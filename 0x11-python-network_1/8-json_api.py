20fe1b10c55bec640349d03f4eb1381d202a2406
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 09:06:40 +0800

    Create 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends POST request to
+        http://0.0.0.0:5000/search_user
+- The letter must be sent in the variable q
+- If no argument given set q=""
+- if response if properly JSON formatted and not empty display the id and name
+-- [<id>] <name>
+-- Display Not a valid JSON if the JSON is invalid
+-- Display No result if the JSON id empty
+"""
+import json
+from sys import argv
+import requests
+
+
+url = 'http://0.0.0.0:5000/search_user'
+try:
+    q = argv[1]
+except IndexError:
+    q = ""
+request = requests.post(url, data=q)
+try:
+    json_dict = json.loads(str(request.text))
+    if json_dict is None:
+        print('No result')
+    else:
+        print('[{}] {}'.format(json_dict[id], json_dict[name]))
+except Exception as e:
+    print('Not a valid JSON')
