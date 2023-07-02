429186716c76bf8ba9baa69642b2e8d5ba0b18ea
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 15:21:21 +0800

    Update to take only the status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | cut -d " " -f 2
+curl -sI "$1" | cut -d ' ' -f 2 | head -n 1
