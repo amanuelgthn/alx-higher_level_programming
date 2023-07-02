738544b02ddeeb7d7126656816683b613503f8a2
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 16:08:49 +0800

    Update curl json 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a JSON POST request to a URL passed as the 1st argument, and displays the body of the response
-curl -s "$1" -d @"$2" -X POST -H "Content-Type: application/json"
+curl -sX POST "$1" -H "Content-Type: application/json" -d @"$1"
