6c9f114269ecbe34d9f07be7b4bc042c9d07360b
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 09:37:15 +0800

    Update 8-json_api.py for q=''

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     request = requests.post(url, data=q)
     try:
         json_dict = request.json()
-        if json_dict is None:
+        if json_dict == {}:
             print('No result')
         else:
             print('[{}] {}'.format(json_dict['id'], json_dict['name']))
