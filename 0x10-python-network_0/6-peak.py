16af8bae9d4cf5ad9d5280e45c9614ca328d6b0d
amanuelgthn <amanuelgthn@gmail.com>
2023-07-02 14:35:02 +0800

    UPdate 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,8 +8,11 @@ def find_peak(list_of_integers):
     """
     finds a peak in a list of integers
     """
+    if list_of_integers == []:
+        return
     for i in range(len(list_of_integers)):
-        if i > 0 and i < len(list_of_integers) - 1:
-            if (list_of_integers[i] >= list_of_integers[i - 1]
-               and list_of_integers[i] >= list_of_integers[i+1]):
+        if i > 0 and i < (len(list_of_integers) - 1):
+            if (list_of_integers[i] > list_of_integers[i - 1]
+               and list_of_integers[i] > list_of_integers[i+1]):
                 return list_of_integers[i]
+    return list_of_integers[0]
