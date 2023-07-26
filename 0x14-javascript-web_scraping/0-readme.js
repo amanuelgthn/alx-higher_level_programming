971541acbb9d45f1eaaaa899388824d0bb2bc42b
Amanuel <amanuel@topformbras.com.cn>
2023-07-26 10:48:13 +0800

    Create 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let file = require('fs');
+
+file.readFile(process.argv[2], function (err, buf) {
+  if (!err) {
+    console.log(buf.toString());
+  } else {
+    console.log(err);
+  }
+});
