6dae4af8670d58ad715ea0253c39984123d2852d
amanuelgthn <amanuelgthn@gmail.com>
2023-07-27 16:10:53 +0800

    fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,13 +6,13 @@ const fs = require('fs');
 const filePath = process.argv[3];
 const url = process.argv[2];
 request(url, (err, response) => {
-    if (err) {
-        console.log(err);
-    } else {
-        const jsonData = JSON.stringify(response);
-        const jsonNew = JSON.parse(jsonData);
-        fs.writeFile(filePath, jsonNew.body, 'utf8', (err) => {
-            if (err) throw err;
-        });
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    const jsonData = JSON.stringify(response);
+    const jsonNew = JSON.parse(jsonData);
+    fs.writeFile(filePath, jsonNew.body, 'utf8', (err) => {
+      if (err) throw err;
+    });
+  }
 });
