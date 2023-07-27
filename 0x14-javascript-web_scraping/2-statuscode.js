284219245c49595b693d9db6b1a73b3eafd64ca7
amanuelgthn <amanuelgthn@gmail.com>
2023-07-27 10:13:33 +0800

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,11 +3,10 @@
 const request = require('request');
 
 const url = process.argv[2];
-console.log(url);
 request(url, (err, response) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: %s', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
