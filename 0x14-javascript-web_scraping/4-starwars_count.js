9282795e08bf17778f52d6c6bb3600071cf348c8
amanuelgthn <amanuelgthn@gmail.com>
2023-07-27 15:36:31 +0800

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,20 @@
 const request = require('request');
 
 const id = 18;
-const urlPeople = 'https://swapi-api.alx-tools.com/api/people';
-const urlId = urlPeople + '/' + id + '/';
-request(urlId, (err, response) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const jsonData = JSON.stringify(response);
-    const jsonNew = JSON.parse(jsonData);
-    const jsonBody = JSON.parse(jsonNew.body);
-    console.log((jsonBody.films.length));
-  }
-});
+const url = process.argv[2];
+if (url !== 'https://swapi-api.alx-tools.com/api/films') {
+  console.log(0);
+} else {
+  const urlPeople = 'https://swapi-api.alx-tools.com/api/people';
+  const urlId = urlPeople + '/' + id + '/';
+  request(urlId, (err, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const jsonData = JSON.stringify(response);
+      const jsonNew = JSON.parse(jsonData);
+      const jsonBody = JSON.parse(jsonNew.body);
+      console.log((jsonBody.films.length));
+    }
+  });
+}
