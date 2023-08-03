8c541143c7980eb48ab4f65f5ae35ac4a76055e0
amanuelgthn <amanuelgthn@gmail.com>
2023-08-03 10:53:43 +0800

    Create 4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    //select the header element and add class "red"
+    $("DIV#toggle_header").click(function() {
+        $("header").toggleClass("red").toggleClass("green");
+    });
