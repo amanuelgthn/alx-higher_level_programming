a3c0285c8d7adb22ad6ceecf9cc25859cb49b125
amanuelgthn <amanuelgthn@gmail.com>
2023-08-03 10:44:15 +0800

    Create 3-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //select the header element and update to red(#FF0000)
+    $("#red_header").click(function() {
+        $("header").css("color", "#FF0000");
+    });
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //select the header element and add class "red"
+    $("DIV#red_header").click(function() {
+        $("header").addClass("red")
+    });
+});
